810f1b22b85509ca25562604f530614bb3c07d5d
klaus <13420134410@163.com>
2021-12-04 14:48:28 +0800

    add two-sum

--- fasttrack/array/1_two_sum_test.go
+++ fasttrack/array/1_two_sum_test.go
@@ -20,3 +20,22 @@ func twoSum(nums []int, target int) []int {
 }
 
 // solution 2 
+
+
+// two-sum 的优化版本，如果数组有序的情况下，应该怎么办？
+int[] twoSum(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+        int sum = nums[left] + nums[right];
+        if (sum == target) {
+            return new int[]{left, right};
+        } else if (sum < target) {
+            left++; // 让 sum 大一点
+        } else if (sum > target) {
+            right--; // 让 sum 小一点
+        }
+    }
+    // 不存在这样两个数
+    return new int[]{-1, -1};
+}
+
